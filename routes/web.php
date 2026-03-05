9cbf60ed85ca4ccb74494812b597afda944061c1
sankhan55 <sana.fatima@netopz.com>
2026-03-06 01:38:42 +0500

    redirectroute changed

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('dashboard');
+
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
