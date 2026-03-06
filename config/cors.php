5244a46302db5f2f0d65b3e950ce002df1154603
sankhan55 <sana.fatima@netopz.com>
2026-03-06 12:18:08 +0500

    work done

--- config/cors.php
+++ config/cors.php
@@ -12,7 +12,7 @@ return [
         'http://127.0.0.1:5000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
-        'https://pdf.apimstec.com',
+        'https://compresspdf.id',
     ],
 
     'allowed_origins_patterns' => [],
